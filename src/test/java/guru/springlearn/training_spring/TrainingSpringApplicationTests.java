8724ba15b1e604e4b34f5964b78e1f622324e3ba
Anna Bezkrovnaya <asayhops@gmail.com>
2020-10-29 12:31:24 +0200

    New project

---
+++ src/test/java/guru/springlearn/training_spring/TrainingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springlearn.training_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
